1e224bd1342629a444cc28762b1144425e6c538d
thephotogenicbug <navinkumarachari@gmail.com>
2022-01-19 22:19:54 +0530

    Create get and post request api end point

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/products', function(){
-  return "Products";
-});
+Route::get('/products', [ProductController::class, 'index']);
+
+Route::post('/products', [ProductController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
